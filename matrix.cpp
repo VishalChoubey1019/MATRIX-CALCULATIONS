1494a2bac47fd0146fa717ea0f4af6d188616d3a
SumanaDas20 <85782793+SumanaDas20@users.noreply.github.com>
2022-10-12 21:13:07 +0530

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -211,7 +211,48 @@ else
   
   }
 
-    
+  cout<<"\nIf you want the addition of those matrixes then type 'yes' \n";
+  cin>>sub;
+  if(sub=="yes" || sub=="Yes")
+  {
+        int m1[3][3], m2[3][3], i, j, m3[3][3];
+        cout<<"\n Enter First Matrix Elements : \n";
+        for(i=0; i<3; i++)
+        {
+                for(j=0; j<3; j++)
+                {
+                        cout<<" ";
+                        cin>>m1[i][j];
+                }
+        }
+        cout<<"\n Enter Second Matrix Elements : \n";
+        for(i=0; i<3; i++)
+        {
+                for(j=0; j<3; j++)
+                {
+                        cout<<" ";
+                        cin>>m2[i][j];
+                }
+        }
+        cout<<"\n Addition of Two Matrices : \n\n";
+        for(i=0; i<3; i++)
+        {
+                for(j=0; j<3; j++)
+                {
+                        m3[i][j]=m1[i][j]+m2[i][j];
+                }
+        }
+        for(i=0; i<3; i++)
+        {
+                cout<<" ";
+                for(j=0; j<3; j++)
+                {
+                        cout<<m3[i][j]<<"\t";
+                }
+                cout<<"\n";
+        }
+  
+  }
   return 0;
 }
 
